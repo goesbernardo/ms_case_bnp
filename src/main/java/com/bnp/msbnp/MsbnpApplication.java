8069699d64bcc56a1992b8c9a902e2ac27ec8618
Bernardo Goes <bernardo.goes@infobase.com.br>
2022-07-11 11:21:09 -0300

    first commit

---
+++ src/main/java/com/bnp/msbnp/MsbnpApplication.java
@@ -0,0 +1,13 @@
+package com.bnp.msbnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbnpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnp/msbnp/MsbnpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnp.msbnp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
